fa29c5c50e272185f09779f8fc03262f43c8c346
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-20 15:37:13 -0300

    feat(src): setting shadcn-ui   Configuring shadcn-ui, to later create components using it. Other adjustments made to lefthook for optimal environment functionality.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 export default function Home() {
-  const text = "";
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h2>Next Prompt Manager</h2>
