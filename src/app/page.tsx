1afb49eb147ac63146d1dd86238c2ad609df228b
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-19 22:18:03 -0300

    feat(project): configuring libraries   Created Prettier configuration, ESLint .vscode, Lefthook. Also created the Docker yml file, added the Prisma library, database configuration, and migration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 export default function Home() {
+  const text = "";
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h2>Next Prompt Manager</h2>
